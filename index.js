666212211f9392d8f1b061f7e1dbbc3ce08e3410
Lynda Starkus <45218202+Lynda-Starkus@users.noreply.github.com>
2022-06-03 19:09:49 +0100

    No need for template in this repo

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();const port = 8000;app.listen(port, () => {
+ console.log(`Welcome to the mern auth tutorial! Server is running on ${port}`)
+});
