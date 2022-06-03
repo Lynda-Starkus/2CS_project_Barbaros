c548a28df207c9afa6a7fd847e0f44b053ee30d0
Lynda Starkus <45218202+Lynda-Starkus@users.noreply.github.com>
2022-06-03 20:02:36 +0100

    updated token auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,36 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const fs = require('fs');
 const {
    createJWT,
-} = require("../utils/auth");exports.signup = (req, res, next) => {
-  let { port, name, password, password_confirmation } = req.body;
-  User.findOne({name: name})
-   .then(user=>{
-      if(user){
-         return res.status(422).json({ errors: [{ user: "name already exists" }] });
-      }else {
+} = require("../utils/auth");
+
+exports.signup = (req, res, next) => {
+  let { name, password, password_confirmation } = req.body;  let errors = [];
+  if (!name) {
+    errors.push({ name: "required" });
+  }  if (!password) {
+    errors.push({ password: "required" });
+  }  if (!password_confirmation) {
+    errors.push({
+     password_confirmation: "required",
+    });
+  }  if (password != password_confirmation) {
+    errors.push({ password: "mismatch" });
+  }  if (errors.length > 0) {
+    return res.status(422).json({ errors: errors });
+  } User.findOne({name: name})
+    .then(user=>{
+       if(user){
+          return res.status(422).json({ errors: [{ user: "name already exists" }] });
+       }else {
+         const port_dispo = fs.readFileSync('../PORT_ASSIGN.txt','utf-8');
          const user = new User({
-           port: port,
+           port: port_dispo,
            name: name,
            password: password,
-         });         bcrypt.genSalt(10, function(err, salt) { bcrypt.hash(password, salt, function(err, hash) {
+         }); bcrypt.genSalt(10, function(err, salt) { bcrypt.hash(password, salt, function(err, hash) {
          if (err) throw err;
          user.password = hash;
          user.save()
@@ -31,6 +47,12 @@ const {
             });
          });
       });
+      const update_port_dispo = Number(port_dispo);
+      update_port_dispo = update_port_dispo + 1;
+
+      const txtWrite = update_port_dispo.toString();
+
+      fs.writeFileSync('./text/writefile.txt', txtWrite);
      }
   }).catch(err =>{
       res.status(500).json({
@@ -39,21 +61,30 @@ const {
   })
 }
 exports.signin = (req, res) => {
-     let { name, password } = req.body;     User.findOne({ name: name }).then(user => {
-       if (!user) {
-         return res.status(404).json({
-           errors: [{ user: "not found" }],
-         });
-       } else {
-          bcrypt.compare(password, user.password).then(isMatch => {
-             if (!isMatch) {
-              return res.status(400).json({ errors: [{ password:
+     let { name, password } = req.body;     let errors = [];
+     if (!name) {
+       errors.push({ name: "required" });
+     }     if (!emailRegexp.test(name)) {
+       errors.push({ name: "invalid name" });
+     }     if (!password) {
+       errors.push({ passowrd: "required" });
+     }     if (errors.length > 0) {
+      return res.status(422).json({ errors: errors });
+     }     User.findOne({ name: name }).then(user => {
+        if (!user) {
+          return res.status(404).json({
+            errors: [{ user: "not found" }],
+          });
+        } else {
+           bcrypt.compare(password, user.password).then(isMatch => {
+              if (!isMatch) {
+               return res.status(400).json({ errors: [{ password:
 "incorrect" }] 
-              });
-             }       let access_token = createJWT(
-         user.name,
-         user._id,
-         3600
+               });
+              }       let access_token = createJWT(
+          user.name,
+          user._id,
+          3600
        );
        jwt.verify(access_token, process.env.TOKEN_SECRET, (err,
 decoded) => {
---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");exports.createJWT = (email, userId, duration) => {
+    const payload = {
+       email,
+       userId,
+       duration
+    };   return jwt.sign(payload, process.env.TOKEN_SECRET, {
+      expiresIn: duration,
+    });
+ };
